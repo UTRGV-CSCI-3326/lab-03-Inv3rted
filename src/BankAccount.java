2a53d7fa5097004681d94c7175bd6d8011b90fac
Inv3rted <84609368+Inv3rted@users.noreply.github.com>
2025-01-31 21:11:19 +0000

    Finished assignment

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,1 +1,12 @@
-
+public class BankAccount{
+    public static void main(String[] args){
+        float balance = 2175.37f;
+        balance -= 302.50f;
+        balance += 50.03f;
+        balance = (balance/2) + 20;
+        balance -= 1;
+        balance *= 2;
+        balance += 1;
+        System.out.println("Bob's current balance is $" + balance);
+    }
+}
